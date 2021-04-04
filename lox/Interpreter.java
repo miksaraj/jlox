fae6a3f49e836a61e449080e16af51a1c477d973
Mikko Rajakangas <mikko.rajakangas@visma.com>
2021-04-04 14:08:23 +0000

    function objects and return statements

--- lox/Interpreter.java
+++ lox/Interpreter.java
@@ -149,6 +149,13 @@ class Interpreter implements Expr.Visitor<Object>,
   }
 
   @Override
+  public Void visitFunctionStmt(Stmt.Function stmt) {
+    LoxFunction function = new LoxFunction(stmt);
+    environment.define(stmt.name.lexeme, function);
+    return null;
+  }
+
+  @Override
   public Void visitIfStmt(Stmt.If stmt) {
     if (isTruthy(evaluate(stmt.condition))) {
       execute(stmt.thenBranch);
@@ -163,6 +170,14 @@ class Interpreter implements Expr.Visitor<Object>,
     Object value = evaluate(stmt.expression);
     System.out.println(stringify(value));
     return null;
+  }
+
+  @Override
+  public Void visitReturnStmt(Stmt.Return stmt) {
+    Object.value = null;
+    if (stmt.value != null) value = evaluate(stmt.value);
+
+    throw new Return(value);
   }
 
   @Override
--- tool/GenerateAst.java
+++ tool/GenerateAst.java
@@ -29,6 +29,7 @@ public class GenerateAst {
       "Expression : Expr expression",
       "Function : Token name, List<Token> params, List<Stmt> body",
       "Print : Expr expression",
+      "Return : Token keyword, Expr value",
       "Var : Token name, Expr initializer",
       "While : Expr condition, Stmt body"
     ));
