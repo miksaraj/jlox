1f2b4a6d612a2481398fb4a7b0f3dff564763948
Mikko Rajakangas <mikko.rajakangas@visma.com>
2021-04-05 13:00:01 +0000

    oop: methods

--- lox/LoxClass.java
+++ lox/LoxClass.java
@@ -6,8 +6,17 @@ import java.util.Map;
 class LoxClass implements LoxCallable {
   final String name;
 
-  LoxClass(String name) {
+  LoxClass(String name, Map<String, LoxFunction> methods) {
     this.name = name;
+    this.methods = methods;
+  }
+
+  LoxFunction findMethod(String name) {
+    if (methods.containsKey(name)) {
+      return methods.get(name);
+    }
+
+    return null;
   }
 
   @Override
