374bfd8088e9ccccfc3237dc9c1a8c1324d25983
Miksa <mikko.s.rajakangas@gmail.com>
2021-12-09 00:14:22 +0200

    fixed package name in generate ast

--- com/jloxlang/tool/GenerateAst.java
+++ com/jloxlang/tool/GenerateAst.java
@@ -46,7 +46,7 @@ public class GenerateAst {
     String path = outputDir + "/" + baseName + ".java";
     PrintWriter writer = new PrintWriter(path, "UTF-8");
 
-    writer.println("package com.miksaraj.lox;");
+    writer.println("package com.jloxlang.lox;");
     writer.println();
     writer.println("import java.util.List;");
     writer.println();
